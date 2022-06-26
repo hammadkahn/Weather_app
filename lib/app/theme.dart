ce16434a7129f9e0be404f41c1819d31434b4ef5
Hammad khan <hammad.khan0312@gmail.com>
2022-06-26 12:26:31 +0500

    App Setup Complete

--- lib/app/theme.dart
+++ lib/app/theme.dart
@@ -3,10 +3,6 @@ import 'package:flutter/material.dart';
 ThemeData theme() {
   return ThemeData(
     scaffoldBackgroundColor: Colors.white,
-    fontFamily: "Muli",
-    appBarTheme: appBarTheme(),
-    textTheme: textTheme(),
-    inputDecorationTheme: inputDecorationTheme(),
-    visualDensity: VisualDensity.adaptivePlatformDensity,
+    fontFamily: "Poppins",
   );
 }
---
+++ lib/screens/splash/splash_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+},
