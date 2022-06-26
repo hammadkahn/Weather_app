be851c2dfa7e7c347e89b01bee4b90cb057cec1d
Hammad khan <hammad.khan0312@gmail.com>
2022-06-26 12:20:46 +0500

    initial commit

---
+++ lib/app/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    fontFamily: "Muli",
+    appBarTheme: appBarTheme(),
+    textTheme: textTheme(),
+    inputDecorationTheme: inputDecorationTheme(),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
